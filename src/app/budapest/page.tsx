affa707c87d07db0541f7d9ab4020ff7e32e79ee
Sukhveer Sangha <sukhveers@push.live>
2023-09-07 21:30:06 +0100

    reusable page layout components

--- src/app/budapest/page.tsx
+++ src/app/budapest/page.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import PageLayout from "@/components/PageLayout";
+import bridge from "../../images/budapest/bridge.jpg";
 
 const Budapest: React.FC = () => {
   return (
-    <div className="headline">
-      <h1>Budapest</h1>
-      <p>
-        Full of historic sites, including Buda Castle, as well as a bustling
-        night life. Lots to explore in a compact city.
-      </p>
-    </div>
+    <>
+      <div className="headline">
+        <h1>Budapest</h1>
+        <p>
+          Full of historic sites, including Buda Castle, as well as a bustling
+          night life. Lots to explore in a compact city.
+        </p>
+      </div>
+      <PageLayout
+        alt1="bridge"
+        img1={bridge}
+        para1="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        alt2="bridge"
+        img2={bridge}
+        para2="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        alt3="bridge"
+        img3={bridge}
+        para3="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      />
+    </>
   );
 };
 
--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,8 +1,47 @@
 import React from "react";
+import Image from "next/image";
 
-const PageLayout: React.FC = () => {
+interface IProps {
+  alt1: string;
+  alt2: string;
+  alt3: string;
+  img1: any;
+  img2: any;
+  img3: any;
+  /**
+   * paragraph
+   */
+  para1: string;
+  para2: string;
+  para3: string;
+}
+
+const PageLayout: React.FC<IProps> = ({
+  alt1,
+  alt2,
+  alt3,
+  img1,
+  img2,
+  img3,
+  para1,
+  para2,
+  para3,
+}) => {
   return (
-
+    <div className="relative flex place-items-center items-center grid grid-flow-col grid-rows-3 grid-cols-1 gap-20 col-span-2 justify-between p-36">
+      <div className="grid grid-cols-2 grid-rows-1 w-full text-center items-center ">
+        <Image src={img1} width={400} height={400} alt={alt1} />
+        <p className=" w-4/5">{para1}</p>
+      </div>
+      <div className="grid grid-cols-2 grid-rows-1 w-full text-center items-center ">
+        <p className=" w-4/5">{para2}</p>
+        <Image src={img2} width={400} height={400} alt={alt2} />
+      </div>
+      <div className="grid grid-cols-2 grid-rows-1 w-full text-center items-center ">
+        <Image src={img3} width={400} height={400} alt={alt3} />
+        <p className=" w-4/5">{para3}</p>
+      </div>
+    </div>
   );
 };
 
