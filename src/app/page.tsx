fc93168fb85811f0aba4b98c4dc2496e7bbe0209
Sukhveer Sangha <sukhveers@push.live>
2023-09-07 15:49:17 +0100

    next link use to navigate to pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,42 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-36">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1>Explore with Sukhveer</h1>
+        <h1 className="text-center">Explore with Sukhveer</h1>
       </div>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <div className="places-div">Rio</div>
-        <div className="places-div">Prague</div>
-        <div className="places-div">Punjab</div>
-        <div className="places-div">Vancouver</div>
-        <div className="places-div">Paris</div>
-        <div className="places-div">Budapest</div>
-        <div className="places-div">Rome</div>
-        <div className="places-div">New York</div>
+        <Link href="/rio">
+          <div className="places-div text-center">Rio</div>
+        </Link>
+        <Link href="/prague">
+          <div className="places-div text-center">Prague</div>
+        </Link>
+        <Link href="/punjab">
+          <div className="places-div text-center">Punjab</div>
+        </Link>
+        <Link href="/vancouver">
+          <div className="places-div text-center">Vancouver</div>
+        </Link>
+        <Link href="/paris">
+          <div className="places-div text-center">Paris</div>
+        </Link>
+        <Link href="/budapest">
+          <div className="places-div text-center">Budapest</div>
+        </Link>
+        <Link href="/rome">
+          <div className="places-div text-center">Rome</div>
+        </Link>
+        <Link href="newyork">
+          <div className="places-div text-center">New York</div>
+        </Link>
       </div>
     </main>
   );
